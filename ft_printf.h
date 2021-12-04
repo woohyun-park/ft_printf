0644e72c8ccab3fac973687e2970ad9f551c75f1
woopark <woopark@student.42seoul.kr>
2021-12-04 13:34:54 +0900

    HMM

--- ft_printf.h
+++ ft_printf.h
@@ -1,23 +1,20 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdio.h>
-
-#include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-#include "libft/libft.h"
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 size_t	ft_strlen(const char *str);
 char	*ft_itoa_u(unsigned int n);
 char	*ft_itoa(int n);
 
-int	write_c(char c);
-int	write_s(char *s);
-int	write_p(unsigned long long int num);
-int	write_diu(long long int num, int flag);
-int	write_x(unsigned long int num, int isUpper, int isX);
+int		write_c(char c);
+int		write_s(char *s);
+int		write_p(unsigned long long int num);
+int		write_diu(long long int num, int flag);
+int		write_x(unsigned long int num, int isUpper, int isX);
 
-int ft_printf(const char *str, ...);
+int		ft_printf(const char *str, ...);
 
 #endif
